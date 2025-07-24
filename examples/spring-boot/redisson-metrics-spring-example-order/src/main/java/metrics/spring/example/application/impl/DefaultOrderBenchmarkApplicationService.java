d8383488439becd79b2188dc6270a2bbe7716f0b
youichi <yoichi.24525@gmail.com>
2025-07-24 17:12:13 +0900

    Enhance DefaultOrderBenchmarkApplicationService with order processing logic
    - Added order creation and retrieval functionality within the `DefaultOrderBenchmarkApplicationService`.
    - Integrated `OrderRepository` for managing order data, including creation, listing, and deletion of orders.
    - Implemented logging for order operations to improve traceability during execution.
    - Updated constructor to include `OrderRepository` as a dependency, enhancing service capabilities.

--- examples/spring-boot/redisson-metrics-spring-example-order/src/main/java/metrics/spring/example/application/impl/DefaultOrderBenchmarkApplicationService.java
+++ examples/spring-boot/redisson-metrics-spring-example-order/src/main/java/metrics/spring/example/application/impl/DefaultOrderBenchmarkApplicationService.java
@@ -1,22 +1,35 @@
 package metrics.spring.example.application.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import metrics.spring.example.application.OrderBenchmarkApplicationService;
+import metrics.spring.example.domain.model.Order;
+import metrics.spring.example.domain.model.OrderRepository;
+import metrics.spring.example.domain.model.account.Account;
 import metrics.spring.example.domain.service.AccountService;
 import metrics.spring.example.interfaces.account.facade.dto.AccountDTO;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
+import java.util.Random;
 
+@Slf4j
 @Component
 public class DefaultOrderBenchmarkApplicationService implements OrderBenchmarkApplicationService {
 
     AccountService accountService;
 
-    public DefaultOrderBenchmarkApplicationService(AccountService accountService) {
+    OrderRepository orderRepository;
+
+    public DefaultOrderBenchmarkApplicationService(
+        AccountService accountService,
+        OrderRepository orderRepository) {
         this.accountService = accountService;
+        this.orderRepository = orderRepository;
     }
 
     @Override
@@ -36,11 +49,11 @@ public class DefaultOrderBenchmarkApplicationService implements OrderBenchmarkAp
 
     @Override
     public void execute(AccountDTO account, Duration duration) {
-        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime startedAt = LocalDateTime.now();
 
         while (true) {
 
-            Duration between = Duration.between(now, LocalDateTime.now());
+            Duration between = Duration.between(startedAt, LocalDateTime.now());
             if (between.compareTo(duration) > 0) {
                 break;
             }
@@ -52,5 +65,37 @@ public class DefaultOrderBenchmarkApplicationService implements OrderBenchmarkAp
     protected void execute(AccountDTO accountDTO) {
         AccountDTO account = accountService.get(accountDTO.getId());
 
+        for (int i = 0; i < 10; i++) {
+            accountService.get(accountDTO.getId());
+        }
+
+        for (int i = 0; i < 100; i++) {
+            Order order = Order
+                .builder()
+                    .id(new Random().nextLong())
+                    .amount(BigDecimal.TEN)
+                    .createdAt(LocalDateTime.now())
+                    .account(
+                        Account
+                            .builder()
+                            .id(account.getId())
+                            .balance(account.getBalance())
+                            .build()
+                    )
+                .build();
+            orderRepository.create(order);
+            orderRepository.get(order.getId());
+            Collection<Order> orders = orderRepository.list(accountDTO.getId());
+
+            log.debug("create order {}", order.getId());
+        }
+
+        Collection<Order> orders = orderRepository.list(accountDTO.getId());
+        orders
+            .stream()
+            .map(Order::getId)
+            .forEach(orderRepository::delete);
+
+        log.debug("task complete.");
     }
 }
